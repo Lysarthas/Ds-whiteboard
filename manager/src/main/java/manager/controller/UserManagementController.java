83855de16daa7f002e583cab0cd6b6641358ea2b
jallychun <jallychun@outlook.com>
2019-10-23 17:03:56 +1100

    map file to project

---
+++ manager/src/main/java/manager/controller/UserManagementController.java
@@ -0,0 +1,77 @@
+package manager.controller;
+
+import java.lang.Thread.UncaughtExceptionHandler;
+import java.rmi.RemoteException;
+import java.time.Instant;
+import java.util.ArrayList;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import manager.drawserver.DrawServer;
+
+public class UserManagementController implements UncaughtExceptionHandler {
+    @FXML
+    private Button startServerButton;
+
+    @FXML
+    private Button stopServerButton;
+
+    @FXML
+    private TextArea serverOutput;
+
+    @FXML
+    private ListView<String> userListView;
+    private ArrayList<String> userList = new ArrayList<String>();
+    private ObservableList<String> observableList = FXCollections.observableArrayList();
+
+    private int port = 8888;
+
+    private DrawServer drawServer;
+    private Thread drawServerThread;
+
+    @FXML
+    public void initialize() throws RemoteException {
+        Thread.setDefaultUncaughtExceptionHandler(this);
+        serverOutput.appendText("Welcome!!\n");
+        userListView.setEditable(false);
+        this.drawServer = DrawServer.newserver(Integer.toString(this.port));
+        this.drawServerThread = new Thread(this.drawServer);
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    @FXML 
+    protected void startServer(ActionEvent event) throws RemoteException {
+        this.drawServerThread.start();
+        this.refreshUserList();
+    }
+
+    @FXML
+    protected void stopServer(ActionEvent event) {
+
+    }
+
+    private void refreshUserList() throws RemoteException {
+        this.userList = this.drawServer.getClientlist();
+        observableList.setAll(this.userList);
+        userListView.setItems(observableList);
+    }
+
+    private void logging(String log) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[").append(Instant.now().toString()).append("] ").append(log).append("\n");
+        serverOutput.appendText(sb.toString());
+    }
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        this.logging(e.getMessage());
+    }
+}
---
+++ manager/src/main/java/manager/drawserver/DrawServer.java
@@ -0,0 +1,95 @@
+package manager.drawserver;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.Locale;
+
+import manager.share.DrawInterface;
+import manager.share.Identity;
+
+public class DrawServer extends UnicastRemoteObject implements DrawInterface, Runnable{
+	
+	private static final long serialVersionUID = 1L;
+	ArrayList<DrawInterface> clients;
+	Identity id;
+	String IP, Port;
+	private static DrawServer serv_ins = null;
+	
+	private DrawServer(String Port) throws RemoteException{
+		clients = new ArrayList<DrawInterface>();
+		this.IP = "localhost";
+		this.Port = Port;
+	}
+	
+	public static DrawServer newserver(String p) throws RemoteException {
+		if(serv_ins == null) {
+			serv_ins = new DrawServer(p);
+		}
+		return serv_ins;
+	}
+	
+	public static DrawServer getInstance() {
+		
+		return serv_ins;
+	}
+	
+	public Identity user() {
+		return id;
+	}
+	
+	public void broadcast(String shape, String timeline, String color, Object o) throws RemoteException{
+		for(int i=0; i < clients.size(); i++) {
+			clients.get(i).drawtask(shape, timeline, color, o);
+		}
+	}
+	
+	public void drawtask(String shape, String timeline, String color, Object o) throws RemoteException{}
+	
+	public boolean login(DrawInterface client, Identity id) throws RemoteException {
+		for(int i=0; i < clients.size(); i++) {
+			if(clients.get(i).user().equals(client.user())) {
+				return false;
+			}
+		}
+		
+		clients.add(client);
+		return true;
+	}
+	
+	public ArrayList<String> getClientlist() throws RemoteException {
+		ArrayList<String> clientname = new ArrayList<>();
+		for(int i=0; i< clients.size(); i++) {
+			clientname.add(clients.get(i).user().getName());
+		}
+		return clientname;
+	}
+	
+	public boolean removeClient(Identity id) {
+		return true;
+	}
+
+	@Override
+	public void run() {
+		try {
+			DrawServer server = getInstance();
+			String url = "rmi://"+this.IP+":"+this.Port+"/RMIServer";
+			LocateRegistry.createRegistry(Integer.parseInt(this.Port));
+			Naming.rebind(url, server);
+			Locale.setDefault(Locale.ENGLISH);
+			// DrawPictureFrame frame = new DrawPictureFrame();
+			// frame.setVisible(true);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
