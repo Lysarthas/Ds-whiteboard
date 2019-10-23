d7a3333b0782e51a1950a272989c9444fe36162a
Liu Yuansan <lysarthas@gmail.com>
2019-10-23 15:52:16 +1100

    new

--- DrawServer.java
+++ DrawServer.java
@@ -4,40 +4,80 @@ import java.rmi.server.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class DrawServer extends UnicastRemoteObject implements DrawInterface{
+public class DrawServer extends UnicastRemoteObject implements DrawInterface, Runnable{
 	
 	private static final long serialVersionUID = 1L;
 	ArrayList<DrawInterface> clients;
-	HashMap<DrawInterface, String> clientname;
+	Identity id;
+	String IP, Port;
+	private static DrawServer serv_ins = null;
 	
-	protected DrawServer() throws RemoteException{
+	public DrawServer(String Port) throws RemoteException{
 		clients = new ArrayList<>();
-		clientname = new HashMap<>();
-	}
-
-	public static void main(String[] args) throws RemoteException, MalformedURLException {
-		Naming.rebind("RMIServer", new DrawServer());
-
+		this.IP = "localhost";
+		this.Port = Port;
 	}
 	
-	public void broadcast(DrawInterface di, String shape, String timeline, String color, Object o) throws RemoteException{
-		String taskclient = clientname.get(di);
+	public static DrawServer newserver(String p) throws RemoteException {
+		if(serv_ins == null) {
+			serv_ins = new DrawServer(p);
+		}
+		return serv_ins;
+	}
+	
+	public static DrawServer getInstance() {
+		
+		return serv_ins;
+	}
+	
+	public Identity user() {
+		return id;
+	}
+	
+	public void broadcast(String shape, String timeline, String color, Object o) throws RemoteException{
 		for(int i=0; i < clients.size(); i++) {
-			DrawInterface client = clients.get(i);
-			if(clientname.get(client).equals(taskclient)) {
-				continue;
-			}
-			else {
-				client.drawtask(shape, timeline, color, o);
-			}
+			clients.get(i).drawtask(shape, timeline, color, o);
 		}
 	}
 	
 	public void drawtask(String shape, String timeline, String color, Object o) throws RemoteException{}
 	
-	public void login(DrawInterface di, String name) {
-		this.clients.add(di);
-		this.clientname.put(di, name);
+	public boolean login(DrawInterface client, Identity id) throws RemoteException {
+		for(int i=0; i < clients.size(); i++) {
+			if(clients.get(i).user().equals(client.user())) {
+				return false;
+			}
+		}
+		
+		clients.add(client);
+		return true;
+	}
+	
+	public ArrayList<DrawInterface> getClientlist() {
+		return this.clients;
+	}
+	
+	public boolean removeClient(Identity id) {
+		return true;
+	}
+
+
+
+	@Override
+	public void run() {
+		try {
+			DrawServer server = getInstance();
+			String url = "rmi://"+this.IP+":"+this.Port+"/RMIServer";
+			Naming.rebind(url, server);
+			
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
