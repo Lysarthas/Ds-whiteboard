d15b5c06faa2445c4f0e7f78b7583a70446e75fc
Liu Yuansan <lysarthas@gmail.com>
2019-10-23 15:52:50 +1100

    newchange

--- DrawInterface.java
+++ DrawInterface.java
@@ -3,8 +3,9 @@ import java.rmi.RemoteException;
 
 public interface DrawInterface extends Remote {
 	
-	void broadcast(DrawInterface di, String shape, String timeline, String color, Object o) throws RemoteException;
-	void drawtask(String shape, String timeline, String color, Object o) throws RemoteException;
-	void login(DrawInterface di, String myname) throws RemoteException;
+	public Identity user() throws RemoteException;
+	public void broadcast(String shape, String timeline, String color, Object o) throws RemoteException;
+	public void drawtask(String shape, String timeline, String color, Object o) throws RemoteException;
+	public boolean login(DrawInterface client, Identity id) throws RemoteException;
 
 }
