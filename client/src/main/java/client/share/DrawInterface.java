d94a17497e5a99c65573849abc69b26fd4e89898
jallychun <jallychun@outlook.com>
2019-10-23 18:29:18 +1100

    add interface

---
+++ client/src/main/java/client/share/DrawInterface.java
@@ -0,0 +1,14 @@
+package client.share;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.security.Identity;
+
+public interface DrawInterface extends Remote {
+	
+	public Identity user() throws RemoteException;
+	public void broadcast(String shape, String timeline, String color, Object o) throws RemoteException;
+	public void drawtask(String shape, String timeline, String color, Object o) throws RemoteException;
+	public boolean login(DrawInterface client, Identity id) throws RemoteException;
+
+}
