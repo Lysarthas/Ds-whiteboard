093928315ac73ee5bd7fcf63449e60d3f6eafd03
Liu Yuansan <lysarthas@gmail.com>
2019-10-24 16:17:06 +1100

    changed

--- manager/src/main/java/rmi/share/DrawInterface.java
+++ manager/src/main/java/rmi/share/DrawInterface.java
@@ -6,8 +6,8 @@ import java.rmi.RemoteException;
 public interface DrawInterface extends Remote {
 	
 	public Identity user() throws RemoteException;
-	public void broadcast(String shape, String timeline, String color, Object o) throws RemoteException;
-	public void drawtask(String shape, String timeline, String color, Object o) throws RemoteException;
+	public void broadcast(Identity editor, String shape, String timeline, Object color, Object o) throws RemoteException;
+	public void drawtask(Identity editor, String shape, String timeline, Object color, Object o) throws RemoteException;
 	public boolean login(DrawInterface client, Identity id) throws RemoteException;
 
 }
