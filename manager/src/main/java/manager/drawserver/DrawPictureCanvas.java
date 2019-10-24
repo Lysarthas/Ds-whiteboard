17e3101771ded1faffec07cde8f8543e2b7aba81
Liu Yuansan <lysarthas@gmail.com>
2019-10-24 17:24:14 +1100

    new

---
+++ manager/src/main/java/manager/drawserver/DrawPictureCanvas.java
@@ -0,0 +1,23 @@
+package manager.drawserver;
+
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.Image;
+
+public class DrawPictureCanvas extends Canvas{
+	private Image image=null;
+	
+	public void setImage(Image image) {
+		this.image=image;
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		g.drawImage(image, 0, 0, null);
+	}
+	
+	public void update(Graphics g) {
+		paint(g);
+	}
+}
+	
